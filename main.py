a1c157a65a933eb9a194cc5e15e7f062977d4c7a
Faruq <farooqshaikh22@ymail.com>
2024-05-18 16:54:20 +0530

    data_ingestion pipeline completed

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.CNNClassifier.logger import logging
+from src.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
